6c051346dd9f989b706c09346fe510103cf929f1
antena-kipas <96196251+antena-kipas@users.noreply.github.com>
2024-11-19 11:04:55 +0700

    menambahkan templates dan membuat folder private

--- app/core/Database.php
+++ app/core/Database.php
@@ -52,4 +52,23 @@ class Database {
         
         $this->stmt->bindValue($param, $value, $type);
     }
+
+    //execute the folder
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    
+    public function single(){
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function rowCount(){
+        return $this->stmt->rowCount();
+    }
 }
--- app/views/gerbang/index.php
+++ app/views/gerbang/index.php
@@ -1,5 +1,13 @@
+<?php
+
+    session_start();
+    require_once '../app/core/session.php'
+    
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +22,7 @@
             min-height: 100vh;
             font-family: 'Inria Serif';
         }
+
         .login-box {
             background-color: #ffffff;
             padding: 20px;
@@ -22,60 +31,89 @@
             max-width: 400px;
             width: 100%;
         }
+
         .form-title {
             color: white;
             text-align: center;
             font-size: 30px;
         }
-        
-        
+
+
         .form-description {
             color: white;
             text-align: center;
             font-size: 25px;
         }
+
         .btn-signin {
             background-color: #14AE5C;
             color: white;
             width: 100%;
         }
+
         .btn-signin:hover {
             background-color: #009951;
         }
+
         .forgot-password {
             text-align: center;
             color: #67A979;
         }
+
         .logo {
             display: block;
             margin: 0 auto 20px;
             width: 200px;
         }
+
+        .form-message {
+            margin: 20px;
+            border-radius: 10px;
+            text-transform: capitalize;
+            max-width: 560px;
+            width: 90%;
+            height: 80px;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+        }
+
+        .form-message-red {
+            background-color: #ffb3b3;
+            border: 2px solid #ff0000;
+        }
+
         label {
             color: black;
         }
     </style>
 </head>
+
 <body>
+    <?php flash('login') ?>
     <div class="login-container text-center">
         <img src="<?= BASEURL; ?>/img/Logo-removebg-preview.png" alt="Logo" class="logo">
         <h1 class="form-title">PUSKESMAS LOHBENER</h1>
         <div>
             <p class="form-description">Aplikasi Laporan Kesehatan Lingkungan</p>
         </div>
-        
-        
         <div class="login-box mx-auto">
-            <form>
+            <form method="post" action="<?= BASEURL; ?>/controllers/users.php">
                 <div class="form-group text-left">
                     <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="Value">
+                    <input type="text" class="form-control" name="name/email" placeholder="Value">
                 </div>
                 <div class="form-group text-left">
                     <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Value">
+                    <input type="password" class="form-control" name="usersPwd" placeholder="Value">
                 </div>
-                <button type="submit" class="btn btn-signin mt-3">Sign In</button>
+                <button type="submit" name="submit" class="btn btn-signin mt-3">Sign In</button>
                 <p class="forgot-password mt-3 text-left">
                     <a href="#" style="color: black;">Forgot password?</a>
                 </p>
@@ -87,4 +125,5 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
